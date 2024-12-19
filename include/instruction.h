57e9814cd5081c3e7e658a5678b9ece7140ea38d
jini1651 <jini1651@daum.net>
2024-12-19 10:36:38 +0900

    init: set up src tree

---
+++ include/instruction.h
@@ -0,0 +1,14 @@
+#execute instruction list
+
+#ifndef MYCLASS_H
+#define MYCLASS_H
+
+class MyClass {
+public:
+    MyClass(int value);
+    void displayValue();
+private:
+    int value;
+};
+
+#endif // MYCLASS_H
---
+++ include/interpreter.h
@@ -0,0 +1,12 @@
+#ifndef MYCLASS_H
+#define MYCLASS_H
+
+class MyClass {
+public:
+    MyClass(int value);
+    void displayValue();
+private:
+    int value;
+};
+
+#endif // MYCLASS_H
